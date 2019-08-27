6f604bf4daceb12b72ccd1bb2d4a19591e30677b
Alexander Mikhailenko <molotok539@gmail.com>
2019-08-27 13:50:45 +0900

    Add base character and gravity(SFML).

---
+++ Story_One_Coube/Character.cs
@@ -0,0 +1,38 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_One_Coube
+{
+    class Character
+    {
+        public RectangleShape Sprite;
+
+        int sizeH = 50;
+        int sizeW = 50;
+
+        float gravity = 1;
+
+        public Character()
+        {
+            Sprite = new RectangleShape(new SFML.System.Vector2f(sizeW, sizeH));
+
+            Sprite.Origin = new SFML.System.Vector2f(sizeW / 2, sizeH / 2);
+
+            Sprite.Position = new SFML.System.Vector2f(Program.width / 2, Program.height / 2);
+        }
+
+        public void Update()
+        {
+            if (Sprite == null) return;
+
+            if(Sprite.Position.Y + sizeH / 2 != Program.height)
+            {
+                Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X, Sprite.Position.Y + gravity);
+            }
+        }
+    }
+}
---
+++ Story_One_Coube/Program.cs
@@ -0,0 +1,46 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_One_Coube
+{
+    class Program
+    {
+        static RenderWindow mainWindow;
+
+        public static uint height = 800;
+        public static uint width = 800;
+
+        static Character character;
+
+        static void Main(string[] args)
+        {
+            mainWindow = new RenderWindow(new SFML.Window.VideoMode(width, height), "Story of one Coube");
+            mainWindow.SetVerticalSyncEnabled(true);
+            mainWindow.Closed += MainWindow_Closed;
+
+            character = new Character();
+
+            while (mainWindow.IsOpen)
+            {
+                mainWindow.DispatchEvents();
+
+                mainWindow.Clear(Color.Black);
+
+                character.Update();
+                
+                mainWindow.Draw(character.Sprite);
+
+                mainWindow.Display();
+            }
+        }
+
+        private static void MainWindow_Closed(object sender, EventArgs e)
+        {
+            mainWindow.Close();
+        }
+    }
+}
